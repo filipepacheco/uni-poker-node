eb849688737cc4f51ada892516e2e4482d821a71
Jayme Gomes <jayme.r.g.n@hotmail.com>
2021-09-29 21:27:54 -0300

    add material

--- socket-app/src/app/app.module.ts
+++ socket-app/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
-import {GameCommandsComponent} from './components/game-commands/game-commands.component';
-import {FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { GameCommandsComponent } from './components/game-commands/game-commands.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+
+
 
 const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameCommandsComponent
+    GameCommandsComponent,
+    HomeComponent,
   ],
   imports: [BrowserModule, SocketIoModule.forRoot(config), FormsModule],
   providers: [],
